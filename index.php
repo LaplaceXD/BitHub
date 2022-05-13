9d96061a36ec0a9bd9add783ba6e44aeb5dcc05f
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-13 08:24:51 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+  include_once("src/components/head.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <?php render_head(); ?>
+  <body>
+    <?php echo "Hello World!"; ?>
+  </body>
+</html>
