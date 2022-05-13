d5658789ef73766f14f38b09a3a3a79b89ee5d57
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-13 13:54:36 +0800

    [Feat][Lex] Added login page

--- index.php
+++ index.php
@@ -1,17 +1,56 @@
-<?php 
-  include_once("src/components/head.php");
-  include_once("src/components/forms.php");
-
-  include_once("src/services/db.php");
-?>
 <!DOCTYPE html>
 <html lang="en">
-  <?php render_head(); ?>
+  <?php
+    require_once("src/components/imports.php");
+    render_head();
+  ?>
   <body>
     <section class="container mt-5">
       <h1 class="text-primary text-center">Welcome to BitHub!</h1>
-      <h2 class="text-center">Sign up</h2>
-      <?php render_registration_form(); ?>
+      <h2 class="text-center">Login</h2>
+      <div class="w-100 container" style="max-width: 768px">
+        <?php 
+          $fields = array("username", "password");
+          $error = "";
+          
+          if(array_any($fields, function ($field) { return !isset($_POST[$field]); })) {
+            render_login_form();
+            goto end;
+          } else {
+            $conn = connect_to_db();
+            if(!$conn) {  
+              $error = "We apologize for the inconvenience. An unexpected error occured.";
+              goto errorForm;
+            }
+
+            $result = get_user($conn, $_POST["username"]);
+            
+            if(!$result) {
+              $error = "Error: ".mysqli_error($conn);
+              goto errorForm;
+            } elseif($result->num_rows === 0) {
+              $error = "Invalid user credentails.";
+              goto errorForm;
+            }
+
+            mysqli_close($conn);
+            $user = mysqli_fetch_assoc($result);
+            
+            if(hash_equals($_POST["password"], $user["Password"])) {
+              $error = "Invalid user credentials.";
+              goto errorForm;
+            }
+            
+            echo "Successful Login.";
+            goto end;
+          }
+
+          errorForm:
+          render_login_form($_POST["username"], $error);
+          end:
+        ?>
+        <p class="w-100 mt-2" style="max-width: 768px;">Don't have an account? <a href="signup.php">Sign up!</a></p>  
+      </div>
     </section>
   </body>
 </html>
