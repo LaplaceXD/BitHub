0855b14dd709039e844d183cd325ac88e23a67e9
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-13 12:47:40 +0800

    [Refactor][Lex] Extracted import statement to one file

---
+++ src/components/imports.php
@@ -0,0 +1,9 @@
+<?php 
+  include_once("src/components/head.php");
+  include_once("src/components/forms.php");
+  
+  include_once("src/services/db.php");
+  include_once("src/services/user.php");
+  
+  include_once("src/utils/array.php");
+?>
