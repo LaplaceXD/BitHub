1f97526d7551f3a22dacc9f007a38365af74b956
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-13 11:59:43 +0800

    [Misc][Lex] Added .some js counterpart in array funcs

---
+++ src/utils/array.php
@@ -0,0 +1,10 @@
+<?php
+  function array_any(array $array, callable $fn) {
+      foreach ($array as $value) {
+          if($fn($value)) {
+              return true;
+          }
+      }
+      return false;
+  }
+?>
