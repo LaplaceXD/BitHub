bdbfcdd479acd011e46b4d159a43e87ea877f738
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-13 12:28:13 +0800

    [Feat][Lex] Added user registration service

---
+++ src/services/user.php
@@ -0,0 +1,16 @@
+<?php
+  function register_user(
+    mysqli $conn,
+    string $user,
+    string $pass,
+    string $fname,
+    string $lname,
+    string $gender,
+    string $dob
+  ): mysqli_result | bool {
+    $sql = "INSERT INTO `user` (`Username`, `Password`, `FirstName`, `LastName`, `BirthDate`, `Gender`) 
+    VALUES ('".$user."', '".password_hash($pass, PASSWORD_DEFAULT)."', '.$fname.', '".$lname."', '".$dob."', '".$gender."')";
+
+    return mysqli_query($conn, $sql);
+  }
+?>
