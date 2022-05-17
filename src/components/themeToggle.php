c67b1b55be2be9fade7a05b5e44887fdfac5ca39
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-18 03:14:04 +0800

    [Misc][Lex] Fixed bug concerning theme icons not rendering

--- src/components/themeToggle.php
+++ src/components/themeToggle.php
@@ -1,8 +1,16 @@
 <?php 
   function render_theme_toggle() {
+    ob_start();
+    include("src/img/icons/sun.svg");
+    $light_icon = ob_get_clean();
+    
+    ob_start();
+    include("src/img/icons/moon.svg");
+    $dark_icon = ob_get_clean();
+
     echo '<button class="theme theme__btn">
-            <div class="light-icon"><?php include "src/img/icons/sun.svg"; ?></div>
-            <div class="dark-icon"><?php include "src/img/icons/moon.svg"; ?></div>
+            <div class="light-icon">'.$light_icon.'</div>
+            <div class="dark-icon">'.$dark_icon.'</div>
           </button>';
   }
 ?>
