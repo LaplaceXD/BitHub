6a81e486e368f6cf269552446bd44f9a02aff7c5
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-18 00:39:45 +0800

    [Feat][Lex] Added css parameter for page specific styles in render_head

--- src/components/head.php
+++ src/components/head.php
@@ -1,5 +1,12 @@
 <?php 
-  function render_head($title = "BitHub - Share Your Ideas") {
+  function render_head($css = null, $title = "BitHub - Share Your Ideas") {
+    $specific_css = "";
+    if($css) {
+      foreach($css as $href) {
+        $specific_css .= '<link href="src/css/'.$href.'.css" rel="stylesheet" type="text/css" />' . "\n";
+      }
+    }
+
     echo '<head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -10,6 +17,9 @@
       <!--CSS-->
       <link href="src/css/resets.css" rel="stylesheet" type="text/css" />
       <link href="src/css/base.css" rel="stylesheet" type="text/css" />
+      
+      <!--SPECIFIC CSS-->
+      '.$specific_css.'
     </head>';
   }
 ?>
