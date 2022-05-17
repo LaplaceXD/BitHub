3dd8efc5d2474b9f3c0a8e6b84222b7e64f54afb
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-18 03:22:27 +0800

    [Fix][Lex] Chagned the implementation of register user to accomodate changes

--- src/services/user.php
+++ src/services/user.php
@@ -3,13 +3,10 @@
     $conn,
     $user,
     $pass,
-    $fname,
-    $lname,
-    $gender,
-    $dob
+    $email
   ) {
-    $sql = "INSERT INTO `user` (`Username`, `Password`, `FirstName`, `LastName`, `BirthDate`, `Gender`) 
-    VALUES ('".$user."', '".password_hash($pass, PASSWORD_DEFAULT)."', '".$fname."', '".$lname."', '".$dob."', '".$gender."')";
+    $sql = "INSERT INTO `user` (`Username`, `Password`, `Email`) 
+    VALUES ('".$user."', '".password_hash($pass, PASSWORD_DEFAULT)."', '".$email."')";
 
     return mysqli_query($conn, $sql);
   }
