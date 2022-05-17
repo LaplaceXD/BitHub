a636e576629cdd1569d1f2f98caa7955aab51416
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-05-18 03:04:49 +0800

    [Feat][Lex] Refactored render registration form

--- src/components/forms.php
+++ src/components/forms.php
@@ -1,61 +1,38 @@
-<?php 
+<?php
+  function render_submit_btn($label) {
+    echo '<button type="submit" class="form__btn">'.$label.'</button>';
+  }
+
+  function render_field($name, $label, $value = "") {
+    if($value != "") $value = 'value="'.$value.'"';
+
+    echo '<div class="form__field">
+        <input class="form__input" type="text" id="'.$name.'" name="'.$name.'" '.$value.'required>
+        <label class="form__label" for="'.$name.'">'.$label.'</label>
+      </div>';
+  }
+
   function render_registration_form(
     $user = "",
-    $fname = "",
-    $lname = "",
-    $gender = "",
-    $dob = "",
+    $email = "",
     $err = ""
   ) {
-    $err = $err = "" ? "" : '<p class="text-danger">'.$err.'</p>';
+    
+    $fields = array(
+      "user" => array("Username", $user),
+      "email" => array("Email", $email),
+      "pass" => array("Password", ""),
+      "confpass" => array("Confirm Password", "")
+    );
 
-    echo '<form class="w-100 container" method="POST" style="max-width: 768px;">
-        <div class="mb-3">
-          <label for="user" class="form-label">Username</label>
-          <input type="text" class="form-control" id="user" name="username" value="'.$user.'" required>
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" name="password" required>
-        </div>
-        <div class="mb-3">
-          <label for="confirm-password" class="form-label">Confirm Password</label>
-          <input type="password" class="form-control" id="confirm-password" name="confirm-password" required>
-        </div>
-        
-        <div class="row mb-3">
-          <div class="col-6">
-            <label for="fname" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="fname" name="firstname" value="'.$fname.'" required>
-          </div>
-          <div class="col-6">
-            <label for="lname" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="lname" name="lastname" value="'.$lname.'" required>
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="email" class="form-label">Email</label>
-          <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-        <div class="row mb-3">
-          <div class="col-6">
-            <label for="gender" class="form-label">Gender</label>
-            <select id="gender" name="gender" class="form-select" value="'.$gender.'" required>
-              <option hidden>Select a Gender</option>
-              <option value="M">Male</option>
-              <option value="F">Female</option>
-              <option value="X">Non-binary</option>
-            </select>
-          </div>
-          <div class="col-6">
-            <label for="dob" class="form-label">Date of Birth</label>
-            <input type="date" class="form-control" id="dob" name="dob" value="'.$dob.'" required>
-          </div>
-        </div>
-
-        '.$err.'
-        <button type="submit" class="btn btn-primary w-100">Submit</button>
-      </form>';
+    echo "<form class='reg__form' action='POST'>";
+    foreach($fields as $name => $content) {
+      echo "\n";
+      render_field($name, $content[0], $content[1]);
+    }
+    echo $err = $err = "" ? "" : "\n".'<p class="form__error">'.$err.'</p>';
+    echo "\n".render_submit_btn("Register");
+    echo "\n</form>";
   }
 
   function render_login_form($user = "", $err = "") {
